29bb95cffe78302a60168600f8060d6914f4e2ec
Eric Tiano <etiano417@gmail.com>
2017-12-11 17:04:43 -0500

    Added tests for useIngredients and toString in InventoryTest

--- unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -3,6 +3,9 @@ package edu.ncsu.csc326.coffeemaker;
 import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
 import junit.framework.TestCase;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class InventoryTest extends TestCase {
 
     private Inventory inventory;
@@ -49,5 +52,31 @@ public class InventoryTest extends TestCase {
         }
 
         assertEquals(20,inventory.getSugar());
+    }
+
+    public void testUseIngredients(){
+        Recipe r1 = mock(Recipe.class);
+        when(r1.getAmtChocolate()).thenReturn(10);
+        when(r1.getAmtCoffee()).thenReturn(10);
+        when(r1.getAmtMilk()).thenReturn(10);
+        when(r1.getAmtSugar()).thenReturn(10);
+
+        assertTrue(inventory.useIngredients(r1));
+    }
+
+    public void testToString(){
+        String s1 = inventory.toString();
+
+        //Checks to see if toString shows the amount of coffee
+        assertTrue(s1.contains("Coffee: 15"));
+
+        //Checks to see if toString shows the amount of chocolate
+        assertTrue(s1.contains("Chocolate: 15"));
+
+        //Checks to see if toString shows the amount of milk
+        assertTrue(s1.contains("Milk: 15"));
+
+        //Checks to see if toString shows the amount of sugar
+        assertTrue(s1.contains("Sugar: 15"));
     }
 }
